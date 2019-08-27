9907c4e09f8af495f7d17f91c93daeb8ec264b71
Stephen Synchronicity <sholsen@alumni.emory.edu>
2019-08-27 11:37:10 -0400

    AddRVstoData: debugging messages, clean up rbind, deal with goog/googl
    AddRVstoData: 
    - debugging messages have been added to better pinpoint where errors occur
    - the way duplicates were filtered retained the older (inaccurate) response variables. This has been reworked by switching the order of the rbind, created a date variables (rather than a datetime) and filtering duplicates in that date var. 
    - regex for GOOG returns entries with GOOGL, this has now been accounted for.

--- JobsScripts/AddRVstoData.R
+++ JobsScripts/AddRVstoData.R
@@ -33,7 +33,7 @@ dat %>% lapply(TSLvars = params$TSLvars, add = add, clm.nms = clm.nms, verbose =
       # rvs <- foreach(x = TSLvars, tsl_amt = purrr::map(1:length(TSLvars), tsl_amt = tsl_amt, function(.x,tsl_amt)return(tsl_amt)), l = purrr::map(1:length(TSLvars), l = l, function(.x,l)return(l)), .inorder = T, .multicombine = T, .combine = cbind, .verbose = T, .packages = c("quantmod","xts","rlang","magrittr", "stringr", "lubridate")) %dopar%
       # rvs <- purrr::pmap(list(x = TSLvars), tsl_amt = tsl_amt, l = l,function(x,tsl_amt,l)
       # rvs <- foreach(x = TSLvars, tsl_amt = purrr::map(1:length(TSLvars), tsl_amt = tsl_amt, function(.x,tsl_amt)return(tsl_amt)), l = purrr::map(1:length(TSLvars), l = l, function(.x,l)return(l)), .inorder = T, .multicombine = T, .combine = cbind, .verbose = T, .packages = c("quantmod","xts","rlang","magrittr", "stringr", "lubridate")) %dopar% 
-      rvs <- foreach(x = TSLvars, y = nms, tsl_amt = purrr::map(1:length(TSLvars), tsl_amt = tsl_amt, function(.x,tsl_amt)return(tsl_amt)), l = purrr::map(1:length(TSLvars), l = l, function(.x,l)return(l)), .inorder = T, .multicombine = T, .combine = cbind, .verbose = F, .packages = c("rlang","magrittr", "stringr", "dplyr", "lubridate")) %dopar% {
+      rvs <- foreach(x = TSLvars, y = nms, tsl_amt = purrr::map(1:length(TSLvars), tsl_amt = tsl_amt, function(.x,tsl_amt)return(tsl_amt)), l = purrr::map(1:length(TSLvars), l = l, function(.x,l)return(l)), .inorder = T, .multicombine = T, .combine = cbind, .verbose = T, .errorhandling = "pass", .packages = c("rlang","magrittr", "stringr", "dplyr", "lubridate")) %dopar% {
         s <- attr(l, "Sym")
         message(paste0(lubridate::now()," ",s," - Begin: ",y))
           x$tsl_amt <- tsl_amt
@@ -41,6 +41,7 @@ dat %>% lapply(TSLvars = params$TSLvars, add = add, clm.nms = clm.nms, verbose =
           .args <- list(v = l[, stringr::str_which(names(l), stringr::regex("date|time|open|high|low|close", ignore_case = T))], .args = x)
           out <- eval(rlang::call2("TSL", !!!.args))
         message(paste0(lubridate::now(), " "," - End: ",y))
+        out
       }#)
     })
     message(paste0("End: ", attr(l, "Sym"), " Elapsed: ",lubridate::as.duration(run.time[3])," Time: ",lubridate::now()))
@@ -51,12 +52,11 @@ dat %>% lapply(TSLvars = params$TSLvars, add = add, clm.nms = clm.nms, verbose =
       out <- xts::cbind.xts(rvs, l)
       dates <- time(out) %>% range()
     } else {
-        td_nm <- stringr::str_extract(names(l), "^time$|^date$") %>% subset(subset = !is.na(.)) %>% .[1]
+        td_nm <- stringr::str_extract(names(l), stringr::regex("^time$|^date$", ignore_case = T)) %>% purrr::keep(~ !is.na(.x)) %>% .[1]
         message(paste0("ncolrv: ",ncol(rvs), "col.nms: ",length(clm.nms)))
         colnames(rvs) <- clm.nms
         rvs <- cbind.data.frame(l, rvs)
         out <- tibbletime::tbl_time(rvs, index = !!td_nm) %>% dplyr::select(!!td_nm, dplyr::everything())
-        dates <- tibbletime::get_index_col(out) %>% range()
         }
     
     
@@ -64,31 +64,28 @@ dat %>% lapply(TSLvars = params$TSLvars, add = add, clm.nms = clm.nms, verbose =
     
   } else out <- l
   attr(out, "Sym") <- s
+  message(paste0("Finding files: ", s))
+  
     .a_f <- list.files("~/R/Quant/PositionData", pattern = s, full.names = T)
-    .rvs <- .a_f[.a_f %>% stringr::str_which("rv\\.csv$")]
+    if (s == "GOOG") .rvs <- .a_f[.a_f %>% stringr::str_which("GOOG(?!L).*rv\\.csv$")] else .rvs <- .a_f[.a_f %>% stringr::str_which("rv\\.csv$")]
     if (HDA::go(.rvs)){
-      .rv_data <- purrr::map(.rvs, ~ readr::read_csv(.x))
+      message(paste0("Reading data: ", s))
+      .rv_data <- readr::read_csv(.rvs)
       purrr::map(.rvs, ~ file.remove(.x))
-      .rv_data <- append(.rv_data, out)
-      out <- data.table::rbindlist(.rv_data) %>% subset(subset = !duplicated(.[["time"]]))
-    } else out <- NULL
-    if (HDA::go(out)) { 
-      #.a_f[.a_f %>% stringr::str_which("rv\\.csv$")] %>% purrr::walk( ~ file.remove(.x))
-      write_csv(out, path = paste0("~/R/Quant/PositionData/",s , min(out[[td_nm]]) %>% lubridate::as_date(), "_", max(out[[td_nm]]) %>% lubridate::as_date(), "_rv.csv"))
+      message(paste0("If duplicates: ", s))
+      if (all(names(.rv_data) == names(out))) .rv_data <- rbind.data.frame(out, .rv_data)
+      message(paste0("Removing duplicates: ", s))
+      td_nm <- rlang::sym(td_nm)
+      out <- .rv_data %>% dplyr::mutate(date = lubridate::as_date(!!td_nm)) %>% subset(subset = !duplicated(.[["date"]])) %>% dplyr::select(- date) %>% dplyr::arrange(!!td_nm)
     }
+    
+      message(paste0("Saving ", s))
+      readr::write_csv(out, path = paste0("~/R/Quant/PositionData/",s , min(out[[td_nm]]) %>% lubridate::as_date(), "_", max(out[[td_nm]]) %>% lubridate::as_date(), "_rv.csv"))
+    
   
   return(out)
 })
 # Re add attributes
 parallel::stopCluster(cl)
-purrr::map(dat, nms = clm.nms, function(.x, nms){
-  att <- attr(.x, "Sym")
-  num <- which(!nms %in% names(.x))
-  if(length(num) > 0) {message(paste0(att, ": NAME CHECK | Missing names are:", nms[num]))}
-})
-dat <- purrr::map2(.x = dat, .y = names(dat), function(.x, .y){
-  attr(.x, "Sym") <- .y
-  return(.x)
-})
 if (stringr::str_detect(deparse(sys.calls()[[sys.nframe()-1]]), "sourceEnv")) save(dat,file = "dat.Rdata")
 HDA::unloadPkgs(c("doParallel","iterators","parallel","foreach"))
