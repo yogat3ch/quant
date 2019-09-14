10a1df5d2f3e21d958e8be76c460920aa434451d
Stephen Synchronicity <sholsen@alumni.emory.edu>
2019-09-14 18:57:33 -0400

    AutomatedOOTDataTest: improve buy order email, fix Purchase order info
    - The buy order email now includes a table of 10 predictions (to ensure consistency and not just a fluke positive)
    - Only those symbols where tsl prediction value is over the percentage threshold are returned
    - they are sorted by the RMSE to the actual trailing stop loss returns
    - the RMSE is reported in addition to the percentage and each prediction table
    - all subsequent functions are fixed to match all new formatting changes.

--- JobsScripts/AutomatedOOTDataTest.R
+++ JobsScripts/AutomatedOOTDataTest.R
@@ -17,67 +17,53 @@ dat <- params$getPositions_new(Positions_v, params)
 # ----------------------- Mon Jun 10 13:57:35 2019 ------------------------#
 # Add Independent variables
 source("~/R/Quant/JobsScripts/AddIVstoData.R", local = T)
+# Add dependent variables because dummyVars depends on it.
+source("~/R/Quant/JobsScripts/AddRVstoNewData.R", local = T)
 # ----------------------- Mon Jun 10 15:37:01 2019 ------------------------#
 # Apply Models to the new data
 source("~/R/Quant/JobsScripts/ApplyModelstoNewData.R", local = T)
-Actions <- purrr::map(dat, function(.x){
-  out <- xts::last(.x) %>% dplyr::select(dplyr::ends_with("pred")) %>% unlist %>% as.character %>% as.numeric
-  names(out) <- xts::last(.x) %>% dplyr::select(dplyr::ends_with("pred")) %>% unlist %>% names
-  
-  return(out)
-}) %>% purrr::map2(.y = best_tsl[names(dat)], function(.x, .y){
-  .tsls <- .y[["tsl_types"]][["tsl"]]
-  .pcts <- .y[["tsl_types"]][["pct"]]
-  out <- list()
-  for (i in seq_along(.tsls)) {
-      out[[i]] <- c(Pred = .x[paste0(.tsls[i],"_pred")], Pct = .pcts[i])
-     
-    
-  }
-  if (out[[i]] > 0) return(out) else return(NULL)
-   
-}) %>% purrr::keep(.p = ~ !is.null(.x))
+library(htmltools)
+preds <- purrr::map(dat, function(.x){
+  .out <- head(.x, 10) %>% dplyr::select(dplyr::ends_with("pred")) %>% dplyr::mutate_if(.pred = ~ is.numeric(.), ~ round(.,4))})
 
-# Send Buy Options via Text
-try({load(file = "~/R/Quant/JobsScripts/prev_buyoption.Rdata")})
-if (all(class(prev_buyoption) != "try-error")) {
-buy_option <- {lubridate::now() - prev_buyoption} %>% lubridate::as.period() > lubridate::hours(1)
-prev_buyoption <- lubridate::now()
-save(prev_buyoption, file = "~/R/Quant/JobsScripts/prev_buyoption.Rdata")
-} else {
-  prev_buyoption <- lubridate::now()
-  save(prev_buyoption, file = "~/R/Quant/JobsScripts/prev_buyoption.Rdata")
-  buy_option <- T
-}
+# Create logical subset of those stocks where the prediction exceeds the percentage gain threshold
+.thresh <- purrr::map2(dat, best_tsl[names(dat)], function(.x, .y){
+  .df <- head(.x, 10)
+  .lgl <- purrr::map2_lgl(.y[['tsl_types']][['pct']], .y[['tsl_types']][['tsl']], .df = .df, function(.x, .y, .df){
+    any(xts::last(.df)[[paste0(.y,"_rv_pred")]] > .x)
+  }) 
+  return(.lgl)
+}) %>% purrr::map(~ any(.)) %>% unlist
+# Create a vector of RMSE values of the predictions vs the actual tsl values
+.rmse <- purrr::map2(dat[.thresh], best_tsl[names(dat[.thresh])], function(.x, .y){
+  .df <- head(.x, 10)
+  .rmse <- purrr::map_dbl(.y[['tsl_types']][['tsl']], .df = .df, function(.x, .df){
+    caret::RMSE(.df[[paste0(.x,"_rv_pred")]], .df[[paste0(.x, "_rv")]])
+  }) 
+  return(.rmse)
+})
+.sort <- .rmse %>% purrr::map(~ pluck(., 1)) %>% unlist %>% sort() %>% names
 
-if (buy_option) {
-
-
-if (length(Actions) > 0) {
+if (length(.sort) > 0) {
   HDA::startPkgs(c("htmltools"))
   googleAuthR::gar_auth(token = "~/R/Quant/JobsScripts/gmailoauth.rds")
   gmailr::gmail_auth("full", id = getOption("googleAuthR.client_id"), secret = getOption("googleAuthR.client_secret"))
   
-  bdy <- paste0(tags$table(
-    purrr::map2(Actions, names(Actions), dat = dat, function(.x, .y, dat){
-      out <- purrr::map(.x, tick = .y, function(.x, tick) {
-        out <- rbind(names(.x) %>% stringr::str_replace("^Pred\\.","") %>% stringr::str_replace("_rv_pred", ""),.x %>% round(3)) %>% apply(2, paste, collapse = ": ")
-        out <- c(Sym = tick,out)}) %>% do.call(rbind,.)  %>% cbind(.,Price = paste0("Price: ", xts::last(dat[[.y]])[["close"]]))
-    }) %>% do.call(rbind,.) %>% apply(1, function(r){
-      if (stringr::str_extract(r[2],"\\d{1,3}\\.\\d{1,3}$") %>% as.numeric > stringr::str_extract(r[3],"\\d{1,3}\\.\\d{1,3}$") %>% as.numeric) {
-        r <- purrr::modify(r,function(.x)paste0("<strong>",.x,"</strong>")) %>% unlist
-        
-      }
-      return(r)
-    }) %>% t %>% htmlTable::htmlTable() %>% htmltools::HTML()
-  ))
+  bdy <- tags$body(
+    tags$table(
+      tags$tr(tags$thead(purrr::map(names(preds[.sort]), ~ tags$td(.,colspan = 2)))),
+      tags$tr(purrr::map(best_tsl[.sort], ~ .x[['tsl_types']] %>% select(tsl, pct, `75%max`)) %>% purrr::map2(.rmse[.sort], ~ tagList(tags$td(paste0(.x[1,1], ": ", .x[1,2], "\n 75%: ", round(.[1,3],2),"\n RMSE: ",round(.y[1],3))), tags$td(paste0(.[2,1], ": ", .[2,2], "\n75%: ", round(.[2,3],2),"\n RMSE: ",round(.y[2],3))))))
+      ,
+      tags$tr(preds[.sort] %>% purrr::map(~ head(.x, 10)) %>% purrr::map(~ tags$td(HTML(knitr::kable(.x, format = "html")), colspan = 2)))
+    )
+  )
   # For later use to send via Twilio
   # cat(bdy, file = "~/R/Quant/JobsScripts/table.html")
   # webshot::webshot("table.html", file = "table.png", selector = "table")
   buy_msg <- gmailr::mime(From="sholsen@alumni.emory.edu",
              To="7818797492@vtext.com;sholsen@alumni.emory.edu",
              subject = "R: Buy Options") %>%
-    gmailr::html_body(body = bdy)#%>% gmailr::attach_file("table.png")
+    gmailr::html_body(body = doRenderTags(bdy))#%>% gmailr::attach_file("table.png")
   gmailr::send_message(buy_msg)
 
   # Give a 30 min window to solicit a response, checking each minute
@@ -103,35 +89,33 @@ if (length(Actions) > 0) {
     Purchase$buy_shares <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Shares\\:\\s?)([0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
     # Get the Trailing Stop Loss types
     Purchase$tsl <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=TSL\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
-    # If tsl is not specified, then find the best option
-    if (!HDA::go(Purchase$tsl)){
-      Purchase$tsl <- purrr::map_chr(Purchase$buy_sms, Actns = Actions, P_tsl = Positions_tsl, function(.x, Actns, P_tsl){
+    # Get a boolean as to whether a purchase should be live or not
+    Purchase$live <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Live\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.logical()
+    # If no tsl are specified, then use the first for all
+    if (!HDA::go(Purchase$tsl)) {
+      Purchase$tsl <- purrr::map2_chr(Purchase$buy_sms, Purchase$tsl, .ptsl = best_tsl, function(.x, .y, .ptsl){
         message(paste0("Finding best TSL for ",.x))
-        tsl <- Actns[[.x]]
-        if (HDA::go(tsl)) {
-          out <- stringr::str_extract(names(tsl[[which.max(purrr::map_dbl(tsl, ~ purrr::pluck(.x, 1)))]])[1],"(?<=Pred\\.)([\\w\\d\\_\\.\\-]+)(?=\\_rv\\_pred)")
+        .out <- .pstl[[.x]][['tsl_types']][['tsl']][1]
+        return(.out)
+      })
+    } else if (any(purrr::map_lgl(Purchase$tsl, ~ is.na(.x)))){
+      Purchase$tsl <- purrr::map2_chr(Purchase$buy_sms, Purchase$tsl, .ptsl = best_tsl, function(.x, .y, .ptsl){
+        message(paste0("Finding best TSL for ",.x))
+        .tsl <- .pstl[[.x]]
+        # If a best_tsl exists and a TSL is not specified
+        if (HDA::go(.tsl) & is.na(.y)) {
+          # Use the first - the one with the highest cumulative returns
+          .out <- .tsl[['tsl_types']][['tsl']][1]
         } else {
-        tsl <- P_tsl[.x][["TSL"]]
-          out <- tsl[which.max(tsl$Cum.Returns), "rowname"]
+          # Otherwise use the tsl specified
+        .out <- .ptsl[.x][["tsl_types"]][['tsl']][.y]
         }
-        return(out)
+        return(.out)
       })
         
-    } else if (any(is.na(purrr::map_lgl(Purchase$tsl)))) {
-      Purchase$tsl <- purrr::map_chr(Purchase$buy_sms[is.na(purrr::map_lgl(Purchase$tsl))], Actns = Actions, P_tsl = Positions_tsl, function(.x, Actns, P_tsl){
-        message(paste0("Finding best TSL for ",.x))
-        tsl <- Actns[[.x]]
-        if (HDA::go(tsl)) {
-          out <- stringr::str_extract(names(tsl[[which.max(purrr::map_dbl(tsl, ~ purrr::pluck(.x, 1)))]])[1],"(?<=Pred\\.)([\\w\\d\\_\\.\\-]+)(?=\\_rv\\_pred)")
-        } else {
-          tsl <- P_tsl[.x][["TSL"]]
-          out <- tsl[which.max(tsl$Cum.Returns), "rowname"]
-        }
-        return(out)
-      })
-    }
+    } 
     # Get whether the order is for the live account or for the paper account.
-    Purchase$live <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Live\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
+    
     # If live is not specified, set all to F (paper)
     if (!HDA::go(Purchase$live)){
     Purchase$live <- rep(F, length(Purchase$buy_syms))  
@@ -149,7 +133,6 @@ if (length(Actions) > 0) {
 } else { message(paste0("No predicted gains as of ",lubridate::now()))}
 # If instructions 
 if (length(Purchase$buy_syms) > 0) {
-  Actions <- Actions[Purchase$buy_syms]
+  preds <- preds[Purchase$buy_syms]
   source("~/R/Quant/JobsScripts/AutomatedBuy.R", local = T)
 }
-} # If previous buy options was offered more than 6 hours ago
