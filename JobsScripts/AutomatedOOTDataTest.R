0dcff92c6f68d529e79fac13b349665aebda0c09
Stephen Synchronicity <sholsen@alumni.emory.edu>
2019-09-19 19:55:47 -0400

    AutomatedOOTDataTest
    - params$gs fixed
    - updates with gmailr package
    - debugging purrr calls in if statements
    - purrr debugging lists
    - new status messages
    - remove no longer needed code

--- JobsScripts/AutomatedOOTDataTest.R
+++ JobsScripts/AutomatedOOTDataTest.R
@@ -1,11 +1,11 @@
 HDA::startPkgs(c("magrittr","tidyverse","rlang"))
 setwd("~/R/Quant/JobsScripts")
 message(paste0("Begin AutomatedStopLoss sourced from ",basename(stringr::str_extract(commandArgs(trailingOnly = FALSE), "(?<=script\\s\\=\\s\\')[A-Za-z0-9\\/\\:\\.\\_]+") %>% .[!is.na(.)]), " at ", lubridate::now()," From location: ",getwd()))
-gs <- googlesheets::gs_url("https://docs.google.com/spreadsheets/d/1Iazn6lYRMhe-jdJ3P_VhLjG9M9vNWqV-riBmpvBBseg/edit#gid=0")# ----------------------- Sun Aug 18 08:53:47 2019 ------------------------#
+# ----------------------- Sun Aug 18 08:53:47 2019 ------------------------#
 # Load Files
 # TODO(could use optimization to only retrieve positions_new once per day)
 try({source("~/R/Quant/JobsScripts/parameters.R")}) # principal and TSLvars
-Personal <- googlesheets::gs_read(gs, ws = "Personal")
+Personal <- googlesheets::gs_read(params$gs, ws = "Personal")
 Positions_v <- Personal$Symbol[- c(1,2)]
 names(Positions_v) <- Positions_v
 load(file = params$paths$best_tsl) # Positions tsl for optimal TSL
@@ -46,8 +46,6 @@ preds <- purrr::map(dat, function(.x){
 
 if (length(.sort) > 0) {
   HDA::startPkgs(c("htmltools"))
-  googleAuthR::gar_auth(token = "~/R/Quant/JobsScripts/gmailoauth.rds")
-  gmailr::gmail_auth("full", id = getOption("googleAuthR.client_id"), secret = getOption("googleAuthR.client_secret"))
   
   bdy <- tags$body(
     tags$table(
@@ -60,6 +58,7 @@ if (length(.sort) > 0) {
   # For later use to send via Twilio
   # cat(bdy, file = "~/R/Quant/JobsScripts/table.html")
   # webshot::webshot("table.html", file = "table.png", selector = "table")
+  gmailr::gm_auth_configure(path = "C:/Users/Administrator/Documents/R/gmailr/credentials.json")
   buy_msg <- gmailr::mime(From="sholsen@alumni.emory.edu",
              To="7818797492@vtext.com;sholsen@alumni.emory.edu",
              subject = "R: Buy Options") %>%
@@ -69,63 +68,66 @@ if (length(.sort) > 0) {
   # Give a 30 min window to solicit a response, checking each minute
   timer_start <- lubridate::now()
   timer_end <- timer_start + lubridate::duration(30, "minutes")
-  response <- gmailr::messages(search = paste0("from:(7818797492@vzwpix.com) label:(INBOX) after:",lubridate::today()))
+  response <- gmailr::gm_messages(search = paste0("from:(7818797492@vzwpix.com) label:(INBOX) after:",lubridate::today()))
   while (lubridate::now() < timer_end & is.null(response[[1]][["messages"]][[1]][["id"]])) {
-    response <- gmailr::messages(search = paste0("from:(7818797492@vzwpix.com) label:(INBOX) after:",lubridate::today()))
+    response <- gmailr::gm_messages(search = paste0("from:(7818797492@vzwpix.com) label:(INBOX) after:",lubridate::today()))
     Sys.sleep(60)
   }
   if (!is.null(response[[1]][["messages"]][[1]][["id"]])) {
   # Retrieve Instructions
-  response <- gmailr::message(response[[1]][["messages"]][[1]][["id"]])
-  quant_lbl <- gmailr::labels()[[1]] %>% .[[purrr::map(., function(.x)stringr::str_detect(.x,"Quant") %>% any) %>% unlist %>% which]] %>% purrr::pluck("id")
+  response <- gmailr::gm_message(response[[1]][["messages"]][[1]][["id"]])
+  quant_lbl <- gmailr::gm_labels()[[1]] %>% .[[purrr::map(.,.f = function(.x)stringr::str_detect(.x,"Quant") %>% any) %>% unlist %>% which]] %>% purrr::pluck("id")
    Purchase <- list()
     if (response %>% purrr::pluck("snippet") %>% stringr::str_detect("None")) {
-      gmailr::modify_message(response$id,add_labels = quant_lbl, remove_labels = "INBOX")
+      gmailr::gm_modify_message(response$id,add_labels = quant_lbl, remove_labels = "INBOX")
       stop("No Buy Orders Issued", .call = F)
     } else {
-    Purchase$buy_syms <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Syms?\\:\\s?)([A-Za-z\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both")
-    Purchase$principals <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Amt\\:\\s?)([0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
+    Purchase$buy_syms <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=[Ss]yms?\\:\\s?)([A-Za-z\\,\\s]+)(?=[Ss]h)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both")
+    Purchase$principals <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=[Aa]mt\\:\\s?)([0-9\\,\\s]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
     #Purchase$principals <- ifelse(!is.na(principals), principals, NULL)
-    Purchase$buy_shares <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Shares\\:\\s?)([0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
+    Purchase$buy_shares <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=[Ss]hares\\:\\s?)([0-9\\,\\s]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
     # Get the Trailing Stop Loss types
-    Purchase$tsl <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=TSL\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
+    Purchase$tsl <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=[Tt][Ss][Ll]\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,\\s]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.numeric
     # Get a boolean as to whether a purchase should be live or not
-    Purchase$live <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=Live\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.logical()
+    Purchase$live <- response %>% purrr::pluck("snippet") %>% stringr::str_match(stringr::regex("(?<=[Ll]ive\\:\\s?)([A-Za-z\\_\\-\\.0-9\\,\\s]+)", ignore_case = T)) %>% as.character %>% .[!duplicated(.)] %>% stringr::str_split(",") %>% unlist %>% trimws(which = "both") %>% as.logical()
     # If no tsl are specified, then use the first for all
     if (!HDA::go(Purchase$tsl)) {
-      Purchase$tsl <- purrr::map2_chr(Purchase$buy_sms, Purchase$tsl, .ptsl = best_tsl, function(.x, .y, .ptsl){
-        message(paste0("Finding best TSL for ",.x))
-        .out <- .pstl[[.x]][['tsl_types']][['tsl']][1]
+      Purchase$tsl <- purrr::map2_chr(Purchase$buy_syms, Purchase$tsl, .ptsl = best_tsl, .f = function(.x, .y, .ptsl){
+        message(paste0("Getting first TSL for ",.x))
+        .out <- .ptsl[[.x]][['tsl_types']][['tsl']][1]
         return(.out)
       })
-    } else if (any(purrr::map_lgl(Purchase$tsl, ~ is.na(.x)))){
-      Purchase$tsl <- purrr::map2_chr(Purchase$buy_sms, Purchase$tsl, .ptsl = best_tsl, function(.x, .y, .ptsl){
+    } else if (any(purrr::map_lgl(Purchase$tsl, ~ is.na(.x) | is.numeric(.x)))) {
+      # # For debugging:
+      # list(.ptsl = best_tsl, .x = Purchase$buy_syms[1], .y = Purchase$tsl[1]) %>% list2env(envir = .GlobalEnv)
+       # rm(.ptsl, .x, .y)
+      Purchase$tsl <- purrr::map2_chr(.x = Purchase$buy_syms, .y = Purchase$tsl, .ptsl = best_tsl, .f = function(.x, .y, .ptsl){
         message(paste0("Finding best TSL for ",.x))
-        .tsl <- .pstl[[.x]]
+        .tsl <- .ptsl[[.x]]
         # If a best_tsl exists and a TSL is not specified
         if (HDA::go(.tsl) & is.na(.y)) {
           # Use the first - the one with the highest cumulative returns
           .out <- .tsl[['tsl_types']][['tsl']][1]
         } else {
           # Otherwise use the tsl specified
-        .out <- .ptsl[.x][["tsl_types"]][['tsl']][.y]
+          .out <- .tsl[["tsl_types"]][['tsl']][.y]
         }
         return(.out)
       })
-        
+      
     } 
     # Get whether the order is for the live account or for the paper account.
     
     # If live is not specified, set all to F (paper)
     if (!HDA::go(Purchase$live)){
     Purchase$live <- rep(F, length(Purchase$buy_syms))  
-    } else if (any(is.na(purrr::map_lgl(Purchase$live)))) {
+    } else if (any(is.na(Purchase$live))) {
       #Otherwise if some values are set and others are not, set those as F
-      Purchase$live[is.na(purrr::map_lgl(Purchase$live))] <- F
+      Purchase$live[is.na(Purchase$live)] <- F
     }
   }
     # Move out of inbox to quant folder
-    gmailr::modify_message(response$id,add_labels = quant_lbl, remove_labels = "INBOX")
+    gmailr::gm_modify_message(response$id,add_labels = quant_lbl, remove_labels = "INBOX")
     
   } else {
     message("Response Timer Timed Out: No Buys issued")
@@ -133,6 +135,6 @@ if (length(.sort) > 0) {
 } else { message(paste0("No predicted gains as of ",lubridate::now()))}
 # If instructions 
 if (length(Purchase$buy_syms) > 0) {
-  preds <- preds[Purchase$buy_syms]
+  message("Initializing Automated Buy Script...")
   source("~/R/Quant/JobsScripts/AutomatedBuy.R", local = T)
 }
